a9aecd40a2a67b9ab0a055203f74cb7d0ae60a1b
OussMac <oussamaimzilene@gmail.com>
2024-06-19 08:23:50 +0100

    Exercice 8 finally done

--- EX08/ft_print_combn.c
+++ EX08/ft_print_combn.c
@@ -6,15 +6,17 @@ void helper_func(int n, int digits, int base,char *v)
 	{
 		write(1, v, n);
 		if (!(v[0] == '9' - n + 1 && v[n-1] == '9'))
+		{
 		write(1, ", ", 2);
 		return;
+		}
 	}
 
 	int i;
 	i = base;
 	while (i <= 10 - n + digits)
 	{
-		v[digits] = '0' + i;
+		v[digits] = 48 + i;
 		helper_func(n, digits + 1, i + 1, v);
 		i++;
 	}
@@ -23,15 +25,16 @@ void helper_func(int n, int digits, int base,char *v)
 void ft_print_combn(int n)
 {
 	if (n < 0 || n > 10)
+	{
 	write(1, "n Has to be 0<n<10", 18);
 	return;
-
+	}
 	char v[10] = {0};
 	helper_func(n, 0, 0, v);
 }
 
 int main()
 {
-	ft_print_combn(2);
+	ft_print_combn(4);
 	return 0;
 }
